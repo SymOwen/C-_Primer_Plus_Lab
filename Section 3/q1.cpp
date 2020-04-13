1033b46ef7126374dae0545f612a4aa98cedc4ec
SymOwen <sym.owen@gmail.com>
2020-04-13 16:19:55 +0800

    first upload

---
+++ Section 3/q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int tran = 12;
+    cout << "Enther the height:____________(in inch)\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+    double height;
+    cin >> height;
+    cout << "Your height is " << height << " inch, which is about " << height*tran << "ft." << endl;
+
+}
